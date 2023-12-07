78664bc4ad22a78192692e6ba24f7db6c8087246
Takis Stamatopoulos <el20096@mail.ntua.gr>
2023-12-07 18:33:55 +0200

    Updated the database
    Added:
    > Title
    > Principal
    > Movie Principal

--- Database_sql_files/ntuaflix-schema.sql
+++ Database_sql_files/ntuaflix-schema.sql
@@ -14,27 +14,50 @@ USE `ntuaflix` ;
 -- ----------------------------------
 
 
--- Movie
-CREATE TABLE IF NOT EXISTS `director` (
-  `director_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `director_first_name` VARCHAR(45) NOT NULL,
-  `director_last_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`director_id`))
+-- Principal
+CREATE TABLE IF NOT EXISTS `principal` (
+  `principal_id` VARCHAR(45) NOT NULL,
+  `principal_name` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`principal_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
+-- Movie Principle
+CREATE TABLE IF NOT EXISTS `movie_principal` (
+	`title_title_id` VARCHAR(60) NOT NULL,
+	`principal_principal_id` VARCHAR(45),
+	PRIMARY KEY (`title_title_id`,`principal_principal_id`),
+    CONSTRAINT `mp_principal_principal`
+		FOREIGN KEY (`principal_principal_id`) REFERENCES `principal`(`principal_id`)
+        ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT `mp_title_title1`
+		FOREIGN KEY (`title_title_id`) REFERENCES `title`(`title_id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
--- Movie
-CREATE TABLE IF NOT EXISTS `movie` (
-  `movie_id` INT NOT NULL AUTO_INCREMENT,
-  `movie_title` VARCHAR(60) NOT NULL,
-  `director_director_id` INT UNSIGNED NOT NULL,
-  `img_link` VARCHAR(45) NOT NULL DEFAULT 'https://picsum.photos/200/300',
-  `description` VARCHAR(500) NOT NULL DEFAULT 'A book description',
-  PRIMARY KEY (`movie_id`),
-  CONSTRAINT `fk_movie_director1`
-    FOREIGN KEY (`director_director_id`)
-    REFERENCES `director` (`director_id`)
+-- Title
+CREATE TABLE IF NOT EXISTS `title` (
+  `title_id` VARCHAR(60) NOT NULL,
+  `title_type` VARCHAR(60) NOT NULL,
+  `title_originalTitle` VARCHAR(60) NOT NULL,
+  `title_poster` VARCHAR(45) NOT NULL,
+  `title_startYear` VARCHAR(45) NOT NULL,
+  `title_endYear` VARCHAR(45),
+  `genre_genre_id` VARCHAR(45) NOT NULL,
+  `AKAs_title_AKAs` VARCHAR(45) NOT NULL,
+  `principal_principal_id` VARCHAR(45) NOT NULL,
+  `title_rateing` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`title_id`),
+  CONSTRAINT `fk_title_principal1`
+    FOREIGN KEY (`principal_principal_id`) REFERENCES `principal` (`principal_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_title_AKAs`
+    FOREIGN KEY (`AKAs_title_AKAs`) REFERENCES `AKAs` (`AKA_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -42,7 +65,7 @@ DEFAULT CHARACTER SET = utf8mb3;
 
 -- Genre
 CREATE TABLE IF NOT EXISTS `genre` (
-  `genre_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `genre_id` VARCHAR(45) NOT NULL,
   `genre_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`genre_id`))
 ENGINE = InnoDB
