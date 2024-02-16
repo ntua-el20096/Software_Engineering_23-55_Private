5f93071f25443bcd432316dfc802155788bd9bab
ntua-el20889 <130045019+ntua-el20889@users.noreply.github.com>
2024-02-16 19:21:35 +0200

    Update serverif.js

--- serverif.js
+++ serverif.js
@@ -1,4 +1,383 @@
+//afro
+// Define an endpoint handler for /searchtitle
+app.get(`${baseURL}/searchtitle`, async (req, res) => {
+  const titlePart = req.body.titlePart; // Extract titlePart from query parameters
+  let format = req.query.format || 'json';
 
+  if(!(format === 'json' || format === 'csv')) {
+    const message = { message: 'Invalid format parameter! format should be json or csv', error: err ? err : '' };
+    return res.status(400).send(message);
+}
+  if (!fileData) {
+    const response = {
+      status: 'failed',
+      message: 'No file uploaded'
+    };
+    if (format === 'csv')
+      return res.status(400).send(converter.csv2json(response));
+    return res.status(400).json(response); // Bad Request: No file uploaded
+  } //checks that it has a file in the body
+
+  if (!titlePart) {
+    if(format === "json"){
+      return res.status(400).json({ status: 'failed', message: 'titlePart is required' });
+    }
+    else{
+      res.status(400).send(converter.json2csv({status: 'failed', message: 'titlePart is required'}));
+    }
+  }
+
+  // Establish a connection to the database
+  const connection = mysql.createConnection(databaseConfig);
+
+  try {
+    const query = `SELECT tb.title_id AS titleID, 
+    tb.title_type AS type, 
+    tb.title_originalTitle AS originalTitle, 
+    tb.title_posterURL AS titlePoster, 
+    tb.title_startYear AS startYear, 
+    tb.title_endYear AS endYear, 
+    tb.title_genre AS genres,
+    tr.rating_avg AS avRating,
+    tr.rating_numVotes AS nVotes
+  FROM title_basics tb
+  LEFT JOIN title_ratings tr ON tb.title_id = tr.title_title_id 
+  WHERE tb.title_originalTitle LIKE ?`;
+  const likeTitlePart = `%${titlePart}%`; // SQL LIKE query format
+
+  const [titleResult] = await connection.promise().query(query,likeTitlePart);
+  var titleObjectList = [];
+  for (var i = 0; i < titleResult.length; i++) {
+    var titleObject = titleResult[i];
+    titleObject.genres = titleObject.genres.split(',').map(genre => ({ genreTitle: genre.trim() }));
+    const akasQuery = `SELECT aka_title AS akaTitle, AKA_region AS regionAbbrev FROM title_AKAs WHERE title_title_id = ?`;
+    const [akasResult] = await connection.promise().query(akasQuery, [titleObject.titleID]);
+    titleObject.titleAkas = akasResult;
+    const principalsQuery = `
+      SELECT 
+          np.principal_id AS nameID, 
+          np.principal_name AS name, 
+          tp.principal_category AS category
+      FROM title_principals tp
+      JOIN principal np ON tp.principal_principal_id = np.principal_id
+      INNER JOIN title_basics tb ON tp.title_title_id = tb.title_id
+      WHERE tb.title_id = ?`;
+  const [principalsResult] = await connection.promise().query(principalsQuery, [titleObject.titleID]);
+  titleObject.principals = principalsResult;
+  titleObjectList.push(titleObject);
+  }
+  
+  if(format === "json"){
+    res.status(200).json({titleObjectList}); // Success: Data uploaded successfully
+  }
+  else{
+    res.status(200).send(converter.json2csv({titleObjectList }));
+  }
+  }
+  catch {
+    console.error('Database error:', error);
+    if(format === "json"){
+      res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+      else{
+        res.status(500).send(converter.json2csv({ message: 'Internal server error', error: error.message }));
+      }
+   
+  }
+  finally {
+    connection.end();
+  }
+
+});
+app.post(`${baseURL}/searchtitle`, async (req, res) => {
+  const titlePart = req.body.titlePart; // Extract titlePart from query parameters
+  let format = req.query.format || 'json';
+
+  if(!(format === 'json' || format === 'csv')) {
+    const message = { message: 'Invalid format parameter! format should be json or csv', error: err ? err : '' };
+    return res.status(400).send(message);
+}
+  if (!fileData) {
+    const response = {
+      status: 'failed',
+      message: 'No file uploaded'
+    };
+    if (format === 'csv')
+      return res.status(400).send(converter.csv2json(response));
+    return res.status(400).json(response); // Bad Request: No file uploaded
+  } //checks that it has a file in the body
+
+  if (!titlePart) {
+    if(format === "json"){
+      return res.status(400).json({ status: 'failed', message: 'titlePart is required' });
+    }
+    else{
+      res.status(400).send( {status: 'failed', message: 'titlePart is required'});
+    }
+  }
+
+  // Establish a connection to the database
+  const connection = mysql.createConnection(databaseConfig);
+
+  try {
+    const query = `SELECT tb.title_id AS titleID, 
+    tb.title_type AS type, 
+    tb.title_originalTitle AS originalTitle, 
+    tb.title_posterURL AS titlePoster, 
+    tb.title_startYear AS startYear, 
+    tb.title_endYear AS endYear, 
+    tb.title_genre AS genres,
+    tr.rating_avg AS avRating,
+    tr.rating_numVotes AS nVotes
+  FROM title_basics tb
+  LEFT JOIN title_ratings tr ON tb.title_id = tr.title_title_id 
+  WHERE tb.title_originalTitle LIKE ?`;
+  const likeTitlePart = `%${titlePart}%`; // SQL LIKE query format
+
+  const [titleResult] = await connection.promise().query(query,likeTitlePart);
+  var titleObjectList = [];
+  for (var i = 0; i < titleResult.length; i++) {
+    var titleObject = titleResult[i];
+    titleObject.genres = titleObject.genres.split(',').map(genre => ({ genreTitle: genre.trim() }));
+    const akasQuery = `SELECT aka_title AS akaTitle, AKA_region AS regionAbbrev FROM title_AKAs WHERE title_title_id = ?`;
+    const [akasResult] = await connection.promise().query(akasQuery, [titleObject.titleID]);
+    titleObject.titleAkas = akasResult;
+    const principalsQuery = `
+      SELECT 
+          np.principal_id AS nameID, 
+          np.principal_name AS name, 
+          tp.principal_category AS category
+      FROM title_principals tp
+      JOIN principal np ON tp.principal_principal_id = np.principal_id
+      INNER JOIN title_basics tb ON tp.title_title_id = tb.title_id
+      WHERE tb.title_id = ?`;
+  const [principalsResult] = await connection.promise().query(principalsQuery, [titleObject.titleID]);
+  titleObject.principals = principalsResult;
+  titleObjectList.push(titleObject);
+  }
+  
+    if(format === "json"){
+      res.status(200).json({titleObjectList});
+    }
+    else{
+      res.status(200).send(converter.json2csv({titleObjectList}));
+    }
+  }
+
+  catch {
+    console.error('Database error:', error);
+    if(format === "json"){
+      res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+    else{
+      res.status(500).send(converter.json2csv({ message: 'Internal server error', error: error.message }));
+    }
+  }
+  finally {
+    connection.end();
+  }
+
+});
+app.get(`${baseURL}/bygenre`, async (req, res) => {
+  const { qgenre, minrating, yrFrom, yrTo } = req.body;
+  let format = req.query.format || 'json';
+
+  if(!(format === 'json' || format === 'csv')) {
+    const message = { message: 'Invalid format parameter! format should be json or csv', error: err ? err : '' };
+    return res.status(400).send(message);
+}
+  if (!fileData) {
+    const response = {
+      status: 'failed',
+      message: 'No file uploaded'
+    };
+    if (format === 'csv')
+      return res.status(400).send(converter.csv2json(response));
+    return res.status(400).json(response); // Bad Request: No file uploaded
+  } //checks that it has a file in the body
+
+  // Start building the query
+  let query = `
+      SELECT 
+          tb.title_id AS titleID, 
+          tb.title_type AS type, 
+          tb.title_originalTitle AS originalTitle, 
+          tb.title_posterURL AS titlePoster, 
+          tb.title_startYear AS startYear, 
+          tb.title_endYear AS endYear, 
+          tb.title_genre AS genres,
+          tr.rating_avg AS avRating
+      FROM title_basics tb
+      LEFT JOIN title_ratings tr ON tb.title_id = tr.title_title_id
+      WHERE tb.title_genre LIKE ? AND tr.rating_avg >= ?
+  `;
+
+  const queryParams = [`%${qgenre}%`, minrating];
+
+  // Add year range conditions if provided
+  if (yrFrom) {
+      query += ` AND tb.title_startYear >= ?`;
+      queryParams.push(yrFrom);
+  }
+  if (yrTo) {
+      query += ` AND tb.title_startYear <= ?`;
+      queryParams.push(yrTo);
+  }
+
+  try {
+      // Establish a connection to the database
+      const connection = mysql.createConnection(databaseConfig);
+
+      // Execute the query
+      const [results] = await connection.promise().query(query, queryParams);
+  var titleObjectList = [];
+  for (var i = 0; i < results.length; i++) {
+    var titleObject = results[i];
+    titleObject.genres = titleObject.genres.split(',').map(genre => ({ genreTitle: genre.trim() }));
+    let akasQuery = `SELECT 
+      aka_title AS akaTitle,
+      AKA_region AS regionAbbrev 
+      FROM title_AKAs 
+      WHERE title_title_id = ?`;
+    const [akasResult] = await connection.promise().query(akasQuery, [titleObject.titleID]);
+    titleObject.titleAkas = akasResult;
+    let principalsQuery = `
+      SELECT 
+          np.principal_id AS nameID, 
+          np.principal_name AS name, 
+          tp.principal_category AS category
+      FROM title_principals tp
+      JOIN principal np ON tp.principal_principal_id = np.principal_id
+      WHERE tp.title_title_id = ?`;
+  const [principalsResult] = await connection.promise().query(principalsQuery, [titleObject.titleID]);
+  titleObject.principals = principalsResult;
+  titleObjectList.push(titleObject);
+  }
+    if(format === "json"){
+      res.status(200).json({titleObjectList});
+    }
+    else{
+      res.status(200).send(converter.json2csv({titleObjectList}));
+    }
+
+    // Close the database connection
+    connection.end();
+  } catch (error) {
+      console.error('Database error:', error);
+      if(format === "json"){
+        res.status(500).json({ message: 'Internal server error', error: error.message });
+      }
+      else{
+        res.status(500).send(converter.json2csv({ message: 'Internal server error', error: error.message}));
+      }
+  }
+});
+
+
+
+app.post(`${baseURL}/bygenre`, async (req, res) => {
+  const { qgenre, minrating, yrFrom, yrTo } = req.body;
+  let format = req.query.format || 'json';
+
+  if(!(format === 'json' || format === 'csv')) {
+    const message = { message: 'Invalid format parameter! format should be json or csv', error: err ? err : '' };
+    return res.status(400).send(message);
+}
+  if (!fileData) {
+    const response = {
+      status: 'failed',
+      message: 'No file uploaded'
+    };
+    if (format === 'csv')
+      return res.status(400).send(converter.csv2json(response));
+    return res.status(400).json(response); // Bad Request: No file uploaded
+  } //checks that it has a file in the body
+
+  // Start building the query
+  let query = `
+      SELECT 
+          tb.title_id AS titleID, 
+          tb.title_type AS type, 
+          tb.title_originalTitle AS originalTitle, 
+          tb.title_posterURL AS titlePoster, 
+          tb.title_startYear AS startYear, 
+          tb.title_endYear AS endYear, 
+          tb.title_genre AS genres,
+          tr.rating_avg AS avRating
+      FROM title_basics tb
+      LEFT JOIN title_ratings tr ON tb.title_id = tr.title_title_id
+      WHERE tb.title_genre LIKE ? AND tr.rating_avg >= ?
+  `;
+
+  const queryParams = [`%${qgenre}%`, minrating];
+
+  // Add year range conditions if provided
+  if (yrFrom) {
+      query += ` AND tb.title_startYear >= ?`;
+      queryParams.push(yrFrom);
+  }
+  if (yrTo) {
+      query += ` AND tb.title_startYear <= ?`;
+      queryParams.push(yrTo);
+  }
+
+  try {
+      // Establish a connection to the database
+      const connection = mysql.createConnection(databaseConfig);
+
+      // Execute the query
+      const [results] = await connection.promise().query(query, queryParams);
+  var titleObjectList = [];
+  for (var i = 0; i < results.length; i++) {
+    var titleObject = results[i];
+    titleObject.genres = titleObject.genres.split(',').map(genre => ({ genreTitle: genre.trim() }));
+    let akasQuery = `SELECT 
+      aka_title AS akaTitle,
+      AKA_region AS regionAbbrev 
+      FROM title_AKAs 
+      WHERE title_title_id = ?`;
+    const [akasResult] = await connection.promise().query(akasQuery, [titleObject.titleID]);
+    titleObject.titleAkas = akasResult;
+    let principalsQuery = `
+      SELECT 
+          np.principal_id AS nameID, 
+          np.principal_name AS name, 
+          tp.principal_category AS category
+      FROM title_principals tp
+      JOIN principal np ON tp.principal_principal_id = np.principal_id
+      WHERE tp.title_title_id = ?`;
+  const [principalsResult] = await connection.promise().query(principalsQuery, [titleObject.titleID]);
+  titleObject.principals = principalsResult;
+  titleObjectList.push(titleObject);
+  }
+    if(format === "json"){
+      res.status(200).json({titleObjectList});
+    }
+    else{
+      res.status(200).send(converter.json2csv({titleObjectList}));
+    }
+
+    // Close the database connection
+    connection.end();
+  } catch (error) {
+    console.error('Database error:', error);
+    if(format === "json"){
+      res.status(500).json({
+        message: 'Internal server error',
+        error: error.message,
+        titleObjectList: []
+      });
+    }
+    else{
+      res.status(500).send(converter.json2csv({ 
+        message: 'Internal server error',
+        error: error.message,
+        titleObjectList: []
+      }));
+    }
+  }
+});
+//afro
 
 
 
